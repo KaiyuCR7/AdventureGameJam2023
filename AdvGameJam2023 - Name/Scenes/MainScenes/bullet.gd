4f8e3bc937fbb76fbc063947518e0cc95809ab0f
Kaiyu Adachi <kaiyuadachi@gmail.com>
2023-06-06 19:50:17 -0700

    Add files via upload

--- AdvGameJam2023 - Name/Scenes/MainScenes/bullet.gd
+++ AdvGameJam2023 - Name/Scenes/MainScenes/bullet.gd
@@ -1,26 +1,16 @@
 extends RigidBody2D
 
 @export var bulletSpeed = 500
-var inputVelocity = Vector2.ZERO
-
-func setBulletSpeed(speed):
-	self.bulletSpeed = speed
-
-func setDvelocity(bulletVel):
-	print(bulletVel)
-	self.inputVelocity = bulletVel
+@export var life_time = 1
 
 func _ready():
 	self.apply_impulse(Vector2(bulletSpeed, 0).rotated(rotation), Vector2())
+	nuke()
 
-func _process(delta):
-#	velocity.x = move_toward(0,inputVelocity.x * bulletSpeed, delta * bulletSpeed)
-#	velocity.y = move_toward(0,inputVelocity.y * bulletSpeed, delta * bulletSpeed)
-#	velocity = inputVelocity
-#	move_and_collide(velocity.normalized() * delta * bulletSpeed)
-	pass
-
+func nuke():
+	await get_tree().create_timer(life_time).timeout
+	queue_free()
 
 func _on_body_entered(body):
 	print("entered body")
-	queue_free()
+	self.hide()
